9656db598916277de720971c052b5c5c5eb0e085
ThiagoPeloso <thiago02peloso@gmail.com>
2025-08-13 10:33:06 -0300

    exo 1

---
+++ exo1.py
@@ -0,0 +1,17 @@
+def fazer_pedido():
+    sair = "sair"
+    x = 0
+    sabores = ["Mussarela","Pepperoni","Quatro Queijos","Calabresa","Frango com Catupiry","Chocolate","Romeu e Julieta"]
+    saboresEscolhidos = []
+    pedido = input("Escolha um sabor ou digite 'sair' para sair:")
+    while pedido != sair:
+        if pedido in sabores:
+            saboresEscolhidos.append(pedido)
+            print(f"Pizza de {pedido} adicionada.")
+        else:
+            print("erro")
+        pedido = input("Escolha um sabor ou digite 'sair' para sair:")
+    print("vc saiu :P")
+    print(f"Seus sabores são:{saboresEscolhidos}")
+
+fazer_pedido()
